41bedbce94a7ba0c8364aa0d1e91888d6cec867f
malburo <tongquocbao0605@gmail.com>
2020-05-14 19:33:32 +0700

    table stories

--- src/components/Tables/TableTransactions/TableTransactions.js
+++ src/components/Tables/TableTransactions/TableTransactions.js
@@ -1,74 +1,61 @@
-// import { Table, Tag, Space } from 'antd';
+import React from "react";
+import { Table, Tag, Space } from "antd";
 
-// const columns = [
-//   {
-//     title: 'Name',
-//     dataIndex: 'name',
-//     key: 'name',
-//     render: text => <a>{text}</a>,
-//   },
-//   {
-//     title: 'Age',
-//     dataIndex: 'age',
-//     key: 'age',
-//   },
-//   {
-//     title: 'Address',
-//     dataIndex: 'address',
-//     key: 'address',
-//   },
-//   {
-//     title: 'Tags',
-//     key: 'tags',
-//     dataIndex: 'tags',
-//     render: tags => (
-//       <>
-//         {tags.map(tag => {
-//           let color = tag.length > 5 ? 'geekblue' : 'green';
-//           if (tag === 'loser') {
-//             color = 'volcano';
-//           }
-//           return (
-//             <Tag color={color} key={tag}>
-//               {tag.toUpperCase()}
-//             </Tag>
-//           );
-//         })}
-//       </>
-//     ),
-//   },
-//   {
-//     title: 'Action',
-//     key: 'action',
-//     render: (text, record) => (
-//       <Space size="middle">
-//         <a>Invite {record.name}</a>
-//         <a>Delete</a>
-//       </Space>
-//     ),
-//   },
-// ];
+const { Column } = Table;
 
-// const data = [
-//   {
-//     key: '1',
-//     name: 'John Brown',
-//     age: 32,
-//     address: 'New York No. 1 Lake Park',
-//     tags: ['nice', 'developer'],
-//   },
-//   {
-//     key: '2',
-//     name: 'Jim Green',
-//     age: 42,
-//     address: 'London No. 1 Lake Park',
-//     tags: ['loser'],
-//   },
-//   {
-//     key: '3',
-//     name: 'Joe Black',
-//     age: 32,
-//     address: 'Sidney No. 1 Lake Park',
-//     tags: ['cool', 'teacher'],
-//   },
-// ];
+const data = [
+  {
+    _id: "5ea91db36df37a026d6eb817",
+    userId: "5ea58cf882aa501c38ca3770",
+    bookId: "5ea64e1994a0de2bb4e83fab",
+    userName: "Tống Quốc Bảo",
+    bookTitle: "You don't know jss",
+    quantity: 2,
+    isComplete: true,
+  },
+  {
+    _id: "5ea91db36df37a026d6eb817",
+    userId: "5ea58cf882aa501c38ca3770",
+    bookId: "5ea64e1994a0de2bb4e83fab",
+    userName: "Độ mixi",
+    bookTitle: "HTML",
+    quantity: 5,
+    isComplete: false,
+  },
+  {
+    _id: "5ea91db36df37a026d6eb817",
+    userId: "5ea58cf882aa501c38ca3770",
+    bookId: "5ea64e1994a0de2bb4e83fab",
+    userName: "Ngọc Trinh",
+    bookTitle: "CSS",
+    quantity: 1,
+    isComplete: "false",
+  },
+];
+
+class TableTransactions extends React.Component {
+  render() {
+    return (
+      <Table dataSource={data}>
+        <Column title="Name" dataIndex="userName" key="username" />
+        <Column title="Book" dataIndex="bookTitle" key="book" />
+        <Column title="Quantity" dataIndex="quantity" key="quantity" />
+        <Column
+          title="Status"
+          dataIndex="isComplete"
+          key="status"
+          render={(text, record) => (
+            <Space size="middle">
+              {record.isComplete ? (
+                <Tag>Đã trả</Tag>
+              ) : (
+                <Tag color="blue">Đang mượn</Tag>
+              )}
+            </Space>
+          )}
+        />
+      </Table>
+    );
+  }
+}
+export default TableTransactions;
---
+++ src/stories/Table.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import TableTransactions from "../components/Tables/TableTransactions/TableTransactions";
+export default { title: "Table" };
+
+export const Transactions = () => <TableTransactions />;
